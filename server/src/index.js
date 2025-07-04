f9acbca168a16e5bf272f01c75f0b95adcbd90ec
ShristiPoudel <shristypoudel31@gmail.com>
2025-07-04 18:57:10 +0545

    connection with databse

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import  "dotenv/config";
+import cors from "cors";
+import pool from "./config/db.js";
+
+
+const app = express();
+const port = process.env.PORT || 8002;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//routes
+// app.get("/",(req,res)=>{
+//     res.send("Backend is working");
+// });
+
+//testing postgress connection
+app.get("/",async(req,res)=>{
+    const result = await pool.query("Select current_database()");
+    res.send(`The database name is: ${result.rows[0].current_database}`)
+});
+
+app.listen(port,()=>{
+    console.log("Server has started");
+});
